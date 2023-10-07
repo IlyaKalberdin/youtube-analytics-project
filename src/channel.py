6b621ab464c1c34698a2abe12623cd0de7fed8de
IlyaKalberdin <145118056+IlyaKalberdin@users.noreply.github.com>
2023-10-07 14:47:10 +0300

    Изменения для Flake

--- src/channel.py
+++ src/channel.py
@@ -5,8 +5,10 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
+
+
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализирует id канала"""
         self.channel_id = channel_id
 
 
